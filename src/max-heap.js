e65656f4eaa6c54038595efbb6013ba979bb07ec
Gerasik <gerasik1992@gmail.com>
2019-03-22 17:07:31 +0300

    first commit

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,11 +2,14 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+		this.root = null;
+		this.parentNodes = [];
 	}
 
 	push(data, priority) {
-		
+		let node = new Node(data, priority);
+		this.insertNode(node);
+		this.shiftNodeUp(node);
 	}
 
 	pop() {
@@ -34,11 +37,46 @@ class MaxHeap {
 	}
 
 	insertNode(node) {
-		
+		if (this.root == null) {
+			this.root = node;
+			this.parentNodes.push(node);
+			return;
+		}
+
+		this.parentNodes[0].appendChild(node);
+		this.parentNodes.push(node);
+
+		if (this.parentNodes[0].left != null && this.parentNodes[0].right != null) {
+			this.parentNodes.shift();
+		}
 	}
 
 	shiftNodeUp(node) {
-		
+		// if (node.parent == null) {
+		// 	this.root = node;
+		// 	return;
+		// }
+		// if (node.priority > node.parent.priority){
+		// 	let nodePosition = this.parentNodes.indexOf(node),
+		// 		nodeParentPosition = this.parentNodes.indexOf(node.parent);
+		// 		this.parentNodes[nodePosition] = node.parent;
+		// 		this.parentNodes[nodeParentPosition] = node;
+		// 	node.swapWithParent();
+		// 	this.shiftNodeUp(node);
+		// }
+		if (node.parent == null) {
+			this.root = node;
+			return;
+		}
+		if (node.priority > node.parent.priority) {
+			let [indexOfNode, indexOfParent] = [this.parentNodes.indexOf(node), 
+				                                this.parentNodes.indexOf(node.parent)];
+			if (~indexOfNode) this.parentNodes[indexOfNode] = node.parent;
+			if (~indexOfParent) this.parentNodes[indexOfParent] = node;
+
+			node.swapWithParent();
+			this.shiftNodeUp(node);
+		}
 	}
 
 	shiftNodeDown(node) {
--- src/node.js
+++ src/node.js
@@ -1,22 +1,77 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data 		= data;
+		this.priority	= priority;
+		this.left  		= null;
+		this.right 		= null;
+		this.parent 	= null;
 	}
 
 	appendChild(node) {
-
+		if (this.left == null){
+			this.left = node;
+			node.parent = this;
+		}else if(this.right == null){
+			this.right = node;
+			node.parent = this;
+		}
 	}
 
 	removeChild(node) {
-
+		if (this.left == node){
+			node.parent = null;
+			this.left = null;
+		} else if (this.right == node){
+			node.parent = null;
+			this.right = null;
+		} else {
+			throw new error('Passed node is not a child');
+		}
 	}
 
 	remove() {
-
+		if(this.parent != null){
+			this.parent.removeChild(this);
+		}
 	}
 
 	swapWithParent() {
-		
+		if(this.parent != null){
+			
+			let oldThisLeft = this.left;
+			let oldThisRight = this.right;
+			let oldParent = this.parent;
+			let oldParPar = this.parent.parent;
+
+			if (this.parent.parent != null){ /*.parent.parent */
+				if(oldParPar.left == oldParent){
+					oldParPar.left = this;
+				}else if (oldParPar.right == oldParent){
+					oldParPar.right = this;
+				}
+
+				this.parent = oldParPar;
+			}
+			if (oldParent.left == this){
+				this.left = oldParent;
+				this.left.parent = this;
+				this.right = this.left.right;
+				if(this.right != null){this.right.parent = this};
+				this.left.left = oldThisLeft;
+				this.left.rigth = oldThisRight;
+				if(this.left.left != null) {this.left.left.parent = this.right;}
+				if(this.left.rigth != null){this.left.rigth.parent = this.right;}
+			}else if (oldParent.right == this){
+				this.right = oldParent;
+				this.right.parent = this;				
+				this.left = this.right.left;
+				if(this.left != null){this.left.parent = this};
+				this.right.left = oldThisLeft;
+				this.right.rigth = oldThisRight;
+				if(this.left.left != null) {this.right.left.parent = this.right;}
+				if(this.left.rigth != null){this.right.rigth.parent = this.right;}
+			}
+		}
 	}
 }
 
